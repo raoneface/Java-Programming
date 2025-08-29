74b945b8c88ffc3681c56d171168125e58b87269
Shubham kumar <raoneface@gmail.com>
2025-08-29 17:19:46 +0530

    uptp type conversion

---
+++ data type variable and arrays/data_type.java
@@ -0,0 +1,83 @@
+/**
+ * Java is Strongly typed langguage
+ * java defined 8 perimitive type of data type ->
+ * integer -> byte (8 bits)(-128 to 127), short (16 bits)(-32768 to 32767),
+ * int (32 bits)( -2147483648 to 2147483647) , long (64
+ * bits)(-9223372036854775808 to 9223372036854775807 )
+ * Floating -> float(32)(4.9e-324 to 1.8e+ 308) and double(64)(1.4e- 045 to 3.4e
+ * + 038)
+ * 
+ * Characters -> char(16 bits )( 0 to 65535)
+ * Boolean -> true and false
+ * java does not support unsigned , positive only integers
+ * 
+ * 8 bit = 1 byte
+ * 
+ * java creation is in unicode
+ * 
+ * integer Literals
+ * 
+ * Hexadeicimal number , octal number , binary
+ * 
+ */
+
+class data_type {
+    public static void main(String[] args) {
+        byte max_byte_value = 127;
+        byte min_byte_value = -128;
+        System.out.println(max_byte_value + min_byte_value);
+
+        short max_shor_value = 32767;
+        short min_shor_value = -32768;
+        System.out.println(max_shor_value + min_shor_value);
+
+        int max_int_value = 2147483647;
+        int min_int_value = -2147483648;
+        System.out.println(max_int_value + min_int_value);
+
+        long long_value1 = 10;
+        long long_value2 = -10;
+        System.out.println(long_value1 + long_value2);
+
+        float float_value1 = 44.77777f;
+        float float_value2 = 44.44444f;
+        System.out.println(float_value1 + float_value2);
+
+        double double_value1 = 44.7777733456434;
+        double double_value2 = 44.4444433456434;
+        System.out.println(double_value1 + double_value2);
+
+        char a = 'a';
+        System.out.println(a);
+
+        char b = 88;
+        // integer convert into char
+        System.out.println(b);
+        char c = 66;
+        System.out.println(c);
+        System.out.println(++c);
+
+        boolean bool = false;
+        System.out.println(bool);
+
+        int Hexamdecimal = 0x2234786;
+        System.out.println(Hexamdecimal);
+
+        int octal = 02;
+        System.out.println(octal);
+
+        long ff = 9223372036854775807L;
+        System.out.println(ff);
+        
+        int binary =  0b1010;
+        System.out.println(binary);
+
+        System.out.println((3343.234/43728.33));
+
+        double aa1 =  3.0;
+        double aa2 = 4.0;
+        // dynamic initialized
+        double aa3 = Math.sqrt(aa1 * aa1 + aa2 * aa2);
+        System.out.println(aa3);
+    }
+}
