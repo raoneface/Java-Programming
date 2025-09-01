c30bad015df2d0a6eda3a6f20f478c1693ee9b31
Shubham Kumar <raoneface@gmail.com>
2025-09-01 21:02:26 +0530

    modulus operator

---
+++ Operators/modulusoperator.java
@@ -0,0 +1,10 @@
+class modulusoperator{
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+
+        // remainder
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
